919af1e9b636b6f9fe8108e5c2fc171f7933c80c
M1ck3yZ <m1ck3yz@hotmail.com>
2021-07-18 10:43:15 -0400

    Update App.js

--- mycarousel/src/App.js
+++ mycarousel/src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 
 function App() {
   const [curTranslate, setTranslate] = useState(0);
-  console.log(curTranslate);
+
   return (
     <div className="App">
       <div className="carousel">
